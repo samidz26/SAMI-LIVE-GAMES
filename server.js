da289bd927c417f994fd2f68138f43b46e916180
samidz26 <samizaouiadz@gmail.com>
2026-09-02 15:06:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(__dirname));
+app.use(express.static("."));
 
 app.get("/", (req, res) => {
-res.sendFile(path.join(__dirname, "index.html"));
+res.sendFile(process.cwd() + "/index.html");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
